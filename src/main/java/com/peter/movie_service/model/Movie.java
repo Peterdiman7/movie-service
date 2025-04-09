6cdd33879b2a71e4fb9563d6b271e6c40e23e9dd
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-04-09 12:06:43 +0300

    Add movie model

---
+++ src/main/java/com/peter/movie_service/model/Movie.java
@@ -0,0 +1,40 @@
+package com.peter.movie_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String director;
+
+    @NotNull
+    private LocalDate releaseDate;
+
+    @NotNull
+    private String genre;
+
+    @NotNull
+    private Double rating;
+
+    @NotNull
+    private String description;
+}
