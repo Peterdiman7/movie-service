f72500b0c3a3106be9caf9d3a29765ce9873eb36
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-04-09 15:52:21 +0300

    Add Home Controller

---
+++ src/main/java/com/peter/movie_service/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.peter.movie_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String HomeControllerHandler() {
+        return "Movie microservice for movie gallery system.";
+    }
+}
