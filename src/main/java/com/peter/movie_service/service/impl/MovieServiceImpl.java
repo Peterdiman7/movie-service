b5c0c367a112c8d239dcbeb344b0d782c7c52596
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-04-09 17:05:18 +0300

    Add movie service

--- src/main/java/com/peter/movie_service/service/impl/MovieServiceImpl.java
+++ src/main/java/com/peter/movie_service/service/impl/MovieServiceImpl.java
@@ -1,22 +1,31 @@
 package com.peter.movie_service.service.impl;
 
+import com.peter.movie_service.dto.MovieRequestDTO;
 import com.peter.movie_service.dto.MovieResponseDTO;
 import com.peter.movie_service.mapper.MovieMapper;
 import com.peter.movie_service.model.Movie;
 import com.peter.movie_service.repository.MovieRepository;
+import com.peter.movie_service.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class MovieServiceImpl {
+public class MovieServiceImpl implements MovieService {
     private final MovieRepository movieRepository;
 
     public List<MovieResponseDTO> getMovies() {
         List<Movie> movies = movieRepository.findAll();
 
         return movies.stream().map(MovieMapper::toDTO).toList();
+    }
+
+    public MovieResponseDTO saveMovie(MovieRequestDTO movieRequestDTO) {
+        Movie newMovie = movieRepository.save(MovieMapper.toModel(movieRequestDTO));
+
+        return MovieMapper.toDTO(newMovie);
     }
 }
